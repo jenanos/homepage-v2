dd65de38a5e73958a194d5e4966a6b1143089ae8
jenanos <jens.osberg@gmail.com>
2025-01-02 15:59:04 +0100

    Increase responsiveness

--- components/ParallaxComponent.tsx
+++ components/ParallaxComponent.tsx
@@ -12,6 +12,7 @@ export default function ParallaxComponent() {
   const backgroundY = useParallax(scrollYProgress, 800);
   const mountainsY = useParallax(scrollYProgress, 100);
   const astronautY = useParallax(scrollYProgress, 1200);
+  const textY = useParallax(scrollYProgress, 2800);
 
   return (
     <div className="w-full h-screen overflow-hidden relative place-items-center">
@@ -34,19 +35,32 @@ export default function ParallaxComponent() {
       ></motion.div>
 
       {/* Astronaut */}
-      <motion.div className="absolute z-10" style={{ y: astronautY }}>
-        <Image src="/Astronaut.png" alt="Astronaut" width={200} height={200} />
+      <motion.div
+        className="absolute z-10 top-[10%] right-[5%] md:top-[30%] md:right-[10%]"
+        style={{ y: astronautY }}
+      >
+        <Image
+          src="/Astronaut.png"
+          className="w-48 h-48 md:w-64 md:h-64 lg:w-72 lg:h-72 xl:w-80 xl:h-80 2xl:w-96 2xl:h-96"
+          alt="Astronaut"
+          width={300}
+          height={300}
+        />
       </motion.div>
 
-      {/* Content */}
-      <div className="z-10">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold">Welcome to the Parallax Page</h1>
-          <p className="text-lg mt-4">
-            Scroll down to see the effect in action!
-          </p>
-        </div>
-      </div>
+      {/* Text-content */}
+      <motion.div
+        className="absolute z-10 top-[30%] md:left-[40%] md:top-[10%]"
+        style={{ y: textY }}
+      >
+        <h1 className="text-4xl font-bold text-white drop-shadow-lg p-4 bg-black bg-opacity-50 rounded-xl text-center">
+          Welcome to my personal space!
+        </h1>
+        <p className="text-lg mt-4 text-white drop-shadow-lg p-4 bg-black bg-opacity-50 rounded-xl text-center">
+          Jens Osberg - lawyer and computer scientist by day, guitarist and
+          coder by night
+        </p>
+      </motion.div>
     </div>
   );
 }
