619f70e7828ecf77462cc2b5709de4edc541627f
Jens Osberg <jens.osberg@gmail.com>
2025-10-23 14:31:51 +0200

    Refactor code structure for improved readability and maintainability

--- components/Other.tsx
+++ components/Other.tsx
@@ -13,15 +13,15 @@ export default function Other() {
       <h2 id="developer-heading" className="p-4 text-4xl text-white">
         Developer
       </h2>
+      <div className="w-3/4 lg:w-1/2 text-white text-center">
+        <p className="py-3">
+          I hold a bachelor’s degree in programming and system architecture and have experience with languages and technologies such as Python, Java, Kotlin, JavaScript, React, Next.js, SQL, and Tailwind.
+        </p>
+        <p className="py-3">
+          While my primary focus is on AI policy and regulation, I use coding as both a learning tool and a hobby. It helps me explore emerging features and technologies while staying connected to my technical roots.
+        </p>
+      </div>
       <div className="flex w-3/4 flex-row justify-evenly text-center flex-wrap">
-        <div className="w-full md:w-96 text-white text-left md:text-center">
-          <p className="py-3">
-            I hold a bachelor’s degree in programming and system architecture and have experience with languages and technologies such as Python, Java, Kotlin, JavaScript, React, Next.js, SQL, and Tailwind.
-          </p>
-          <p className="py-3">
-            While my primary focus is on AI policy and regulation, I use coding as both a learning tool and a hobby. It helps me explore emerging features and technologies while staying connected to my technical roots.
-          </p>
-        </div>
         <PrevSite />
       </div>
     </section>
